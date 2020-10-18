e3668fc18caf2531e602b19624c7d57a001e05c4
AryanMangale <aryan.a.mangale@gmail.com>
2020-10-18 11:18:30 +0200

    Added yaourt installer script

---
+++ yaourt-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install package-query
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+rm -rf package-query
+
+# Install yaourt
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+rm -rf yaourt
