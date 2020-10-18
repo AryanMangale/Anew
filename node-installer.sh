06530890a3c18927e381738b9e2a431bb90714c4
AryanMangale <aryan.a.mangale@gmail.com>
2020-10-18 14:36:17 +0200

    Add script to install node and nvm

---
+++ node-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install nvm, a useful tool for managing and installing different versions of node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+
+# This is required to run nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install the latest LTS version of node
+nvm install v12.19.0
+
+source ~/.bashrc
+node -v
+npm -v
